022e8f88bded4c3e2efb3a4fcc3c08f06dacf982
Justin Brathwaite <jbrath110@yahoo.com>
2018-02-25 19:46:07 -0500

    Two sum algo

---
+++ Algorithms/Easy/TwoSumClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+namespace Algorithms.Easy
+{
+
+    /*  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        Given nums = [2, 7, 11, 15], target = 9,
+
+        Because nums[0] + nums[1] = 2 + 7 = 9,
+        return [0, 1].
+
+    */
+
+    public class TwoSumClass
+    {
+        public TwoSumClass()
+        {
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            for (var x = 0; x < nums.Length; x++)
+            {
+                hashtable.Add(nums[x], x);
+                if (hashtable.ContainsKey((target - nums[x])) && target - nums[x] != nums[x])
+                {
+                    return new int[] { (int)hashtable[target - nums[x]], (int)hashtable[nums[x]] };
+                }
+
+            }
+
+            throw new ApplicationException("No two sum solution with the provided input");
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Algorithms.Easy;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSumClass test = new TwoSumClass();
+            int[] nums = new int []{ 2, 7, 11, 15 };
+            int target = 9;
+           var answer= test.TwoSum(nums, target);
+            Console.WriteLine(answer[0]+" "+answer[1]);
+        }
+    }
+}
