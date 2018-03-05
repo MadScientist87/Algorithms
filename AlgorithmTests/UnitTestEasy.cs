c45979e4dd8b8f4e67bc8b684e32acce235f4f3b
Justin Brathwaite <jbrath110@yahoo.com>
2018-03-05 15:48:35 -0500

    add unit tests

---
+++ AlgorithmTests/UnitTestEasy.cs
@@ -0,0 +1,42 @@
+using Algorithms.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class UnitTestEasy
+    {
+        [TestMethod]
+        public void RomanToInteger()
+        {
+            RomanToInteger romanToInteger = new RomanToInteger();
+            Assert.AreEqual(romanToInteger.RomanToInt("MDCCLXXVI"), 1776);
+        }
+
+        [TestMethod]
+        public void TwoSum()
+        {
+            TwoSumClass twoSumClass = new TwoSumClass();
+            int[] nums = new int[] { 800, 4, 800, 21 };
+            int target = 821;
+            var result = twoSumClass.TwoSum(nums, target);
+            CollectionAssert.AreEqual(result, new int[] { 0, 3 });
+        }
+
+        [TestMethod]
+        public void ReverseInteger()
+        {
+            ReverseInteger reverseInteger = new ReverseInteger();
+            int num = 123;
+            Assert.AreEqual(reverseInteger.Reverse(num), 321);
+        }
+
+        [TestMethod]
+        public void IsPalindromeInteger()
+        {
+            IsPalindromeInteger isPalindromeInteger = new IsPalindromeInteger();
+            Assert.IsTrue(isPalindromeInteger.IsPalindrome(303));
+        }
+
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,19 +7,6 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            // TwoSumClass test = new TwoSumClass();
-            // int[] nums = new int []{ 800, 4, 800, 21 };
-            // int target = 821;
-            //var answer= test.TwoSum(nums, target);
-            //Console.WriteLine(answer[0]+" "+ answer[1]);
-
-            //ReverseInteger test = new ReverseInteger();
-            //int num = 123;
-            //Console.WriteLine(test.Reverse(num)); 
-
-            //IsPalindromeInteger test = new IsPalindromeInteger();
-            //test.IsPalindrome(303);
-
             RomanToInteger test = new RomanToInteger();
             Console.WriteLine(test.RomanToInt("MDCCLXXVI"));
         }
