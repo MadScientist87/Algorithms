605b1ee65a0b853dcacdb004f2ee91c3f808be83
jbrath200 <justin_brathwaite@comcast.com>
2019-05-02 17:23:34 -0400

    Update with Amazon Test scaffold

---
+++ AlgorithmTests/UnitTestAmazon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class UnitTestAmazon
+    {
+        [TestMethod]
+        public void FirstMethod()
+        {
+           
+        }
+    }
+}
