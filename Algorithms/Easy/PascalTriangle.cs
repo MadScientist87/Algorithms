8e5871e7fdf912c0ae85abc6799a38b5edd1485f
jbrath200 <jbrath110@yahoo.com>
2019-05-01 22:08:58 -0400

    Pascal triangle

---
+++ Algorithms/Easy/PascalTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Easy
+{
+    public class PascalTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+
+            var mainList = new List<int>();
+
+            for(var x = 0; x< numRows;x++)
+            {
+                var pre = new List<int>();
+                var newList = new List<int>();
+                for (var j = 1;j < pre.Count-1;j++)
+                {
+                    var sum = pre[j] + pre[j + 1];
+                    newList.Add(sum);
+                }
+            }
+        }
+    }
+}
