d7507ecbc93eb773c85255b27963aefbd0f83631
jbrath200 <jbrath110@yahoo.com>
2019-05-28 08:29:03 -0400

    Add new Techniques

---
+++ AlgorithmTests/UnitTestTechnique.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using Algorithms.Technique;
+namespace AlgorithmTests
+{
+    [TestClass]
+    public class UnitTestTechnique
+    {
+
+        [TestMethod]
+        public void MaxSum()
+        {
+            int[] arr = { 1, 4, 2, 10, 2, 3, 1, 0, 20 };
+            int k = 4;
+            int n = arr.Length;
+            SlidingWindow SlidingWindow = new SlidingWindow();
+            SlidingWindow.maxSum(arr, n, k);
+        }
+
+        [TestMethod]
+        public void MergeSort()
+        {
+
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            MergeSort ob = new MergeSort();
+            ob.sort(arr, 0, arr.Length - 1);
+
+        }
+    }
+}
